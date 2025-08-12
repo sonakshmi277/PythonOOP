cf0cad6037984aeb72b4c99d3022e536175811f3
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2025-08-12 23:36:16 +0530

    Revision

--- basicObjCreation.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self,name):
-        self.name=name
-    def set_name(self,new_name):
-        self.name=new_name
-p=Person("Ria")
-print(p.name)
-p.set_name("Mahir")
-print(p.name)
